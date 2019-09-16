481fec9c57b1f9a642ce83a06f0b73c7b71b2404
joannaarroyo <joliuac@gmail.com>
2019-09-16 13:05:15 -0700

    added files from starter code

---
+++ src/components/App/app.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  incrementCounter = e => {
+    e.preventDefault();
+    this.setState(previousState => ({
+      counter: previousState.counter + 1,
+    }));
+  };
+
+  decrementCounter = e => {
+    e.preventDefault();
+    this.setState(previousState => ({
+      counter: previousState.counter - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>{this.state.counter}</h4>
+        <button onClick={this.incrementCounter}> + 1 </button>
+        <button onClick={this.decrementCounter}> - 1 </button>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Counter />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <header>
+    <h1>Jo's Header</h1>
+  </header>
+);
